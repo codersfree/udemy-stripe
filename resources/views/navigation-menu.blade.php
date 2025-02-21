17068b288c56fcdbc6f9c234f2804893f370b8f5
Victor Arana <victor@codersfree.com>
2025-02-21 12:26:54 -0500

    modificar plantila jetstream

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,3 +1,13 @@
+@php
+    $links = [
+        [
+            'name' => 'Dashboard',
+            'url' => route('dashboard'),
+            'active' => request()->routeIs('dashboard'),
+        ],
+    ];
+@endphp
+
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -12,9 +22,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                    @foreach ($links as $link)
+                        <x-nav-link href="{{ $link['url'] }}" :active="$link['active']">
+                            {{ $link['name'] }}
+                        </x-nav-link>
+                    @endforeach
                 </div>
             </div>
 
@@ -92,9 +104,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+
+            @foreach ($links as $link)
+
+                <x-responsive-nav-link href="{{ $link['url'] }}" :active="$link['active']">
+                    {{ $link['name'] }}
+                </x-responsive-nav-link>
+
+            @endforeach
 
             @guest
             
